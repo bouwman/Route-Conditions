2ae4d4655bc74f461ddb8c39121aff93abf0b866
Tassilo Bouwman <t.bouwman@me.com>
2023-05-19 16:41:24 +0200

    Initial Commit

---
+++ Route Conditions/Route_ConditionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Route_ConditionsApp.swift
+//  Route Conditions
+//
+//  Created by Tassilo Bouwman on 19.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Route_ConditionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
