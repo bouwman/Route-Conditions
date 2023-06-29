62908e128311d3371f17d020eb0dafdad372c59e
Tassilo Bouwman <t.bouwman@me.com>
2023-06-29 10:09:32 +0200

    enhane map marker

---
+++ Route Conditions/View/Helper/WeatherMarker.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherMarker.swift
+//  Route Conditions
+//
+//  Created by Tassilo Bouwman on 28.06.23.
+//
+
+import SwiftUI
+
+struct WeatherMarker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherMarker()
+}
