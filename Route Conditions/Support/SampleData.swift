19e2ca50df26714584f066ef7386dec75293c698
Tassilo Bouwman <t.bouwman@me.com>
2023-07-02 12:47:11 +0200

    add sample data

--- Route Conditions/Support/SampleData.swift
+++ Route Conditions/Support/SampleData.swift
@@ -85,3 +85,104 @@ extension CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
+
+extension CustomWaypoint {
+    static var route: [CustomWaypoint] {
+        return [
+            CustomWaypoint(coordinate: CLLocationCoordinate2DMake(49.0084, -2.5381), position: 1), // Start point (France)
+            CustomWaypoint(coordinate: CLLocationCoordinate2DMake(49.4345, -2.6253), position: 2), // Waypoint 1
+            CustomWaypoint(coordinate: CLLocationCoordinate2DMake(49.9094, -2.7597), position: 3), // Waypoint 2
+            CustomWaypoint(coordinate: CLLocationCoordinate2DMake(50.3466, -2.7941), position: 4), // Waypoint 3
+            CustomWaypoint(coordinate: CLLocationCoordinate2DMake(50.7167, -1.8806), position: 5) // End point (UK)
+        ]
+    }
+}
+
+extension WeatherWaypoint {
+    static func createWeatherWaypoints() -> [WeatherWaypoint] {
+        var weatherWaypoints: [WeatherWaypoint] = []
+        let customWaypoints = CustomWaypoint.route
+        
+        var currentDate = Date()
+        for i in 1..<(customWaypoints.count - 1) {
+            let waypoint1 = customWaypoints[i]
+            let waypoint2 = customWaypoints[i + 1]
+            let distance = WeatherWaypoint.calculateDistanceBetweenWaypoints(waypoint1, waypoint2)
+            let numberOfWeatherWaypoints = Int(distance.converted(to: UnitLength.meters).value / WeatherWaypoint.distanceBetweenWeatherWaypoints.converted(to: UnitLength.meters).value)
+            
+            for j in 0..<numberOfWeatherWaypoints {
+                let fraction = Double(j + 1) / Double(numberOfWeatherWaypoints + 1)
+                let latitude = waypoint1.coordinate.latitude + (waypoint2.coordinate.latitude - waypoint1.coordinate.latitude) * fraction
+                let longitude = waypoint1.coordinate.longitude + (waypoint2.coordinate.longitude - waypoint1.coordinate.longitude) * fraction
+                let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+                var weatherWaypoint = WeatherWaypoint(
+                    coordinate: coordinate,
+                    position: weatherWaypoints.count + 1,
+                    date: currentDate
+                )
+                
+                let weatherObjects = Weather.generateWeatherObjects(currentDate: currentDate, coordinate: coordinate)
+                weatherWaypoint.weather = weatherObjects
+                
+                weatherWaypoints.append(weatherWaypoint)
+                
+                // Increment the date by one hour
+                currentDate.addTimeInterval(3600)
+            }
+        }
+        
+        return weatherWaypoints
+    }
+    
+    private static func calculateDistanceBetweenWaypoints(_ waypoint1: CustomWaypoint, _ waypoint2: CustomWaypoint) -> Measurement<UnitLength> {
+        let location1 = CLLocation(latitude: waypoint1.coordinate.latitude, longitude: waypoint1.coordinate.longitude)
+        let location2 = CLLocation(latitude: waypoint2.coordinate.latitude, longitude: waypoint2.coordinate.longitude)
+        let distance = location1.distance(from: location2)
+        return Measurement(value: distance, unit: UnitLength.meters)
+    }
+    
+    static var distanceBetweenWeatherWaypoints: Measurement<UnitLength> {
+        let distances = CustomWaypoint.route.dropFirst().dropLast().map { waypoint -> Measurement<UnitLength> in
+            let nextWaypoint = CustomWaypoint.route[waypoint.position]
+            return calculateDistanceBetweenWaypoints(waypoint, nextWaypoint)
+        }
+        
+        let totalDistance = distances.reduce(Measurement(value: 0, unit: UnitLength.meters), +)
+        let weatherWaypointCount = 20
+        let distanceBetweenWeatherWaypoints = totalDistance / Double(weatherWaypointCount + 1)
+        return distanceBetweenWeatherWaypoints
+    }
+}
+
+extension Weather {
+    static func generateWeatherObjects(currentDate: Date, coordinate: CLLocationCoordinate2D) -> [Weather] {
+        var weatherObjects: [Weather] = []
+        var date = currentDate
+        for _ in 0..<168 {
+            let weather = Weather()
+            
+            weather.coordinate = coordinate
+            weather.date = date
+            
+            // Generate random weather data
+            weather.temperature.air = Measurement(value: Double.random(in: 10...25), unit: UnitTemperature.celsius)
+            weather.temperature.water = Measurement(value: Double.random(in: 10...20), unit: UnitTemperature.celsius)
+            weather.wind.direction = Measurement(value: Double.random(in: 0...360), unit: UnitAngle.degrees)
+            weather.wind.speed = Measurement(value: Double.random(in: 5...20), unit: UnitSpeed.knots)
+            weather.current.direction = Measurement(value: Double.random(in: 0...360), unit: UnitAngle.degrees)
+            weather.current.speed = Measurement(value: Double.random(in: 0...2), unit: UnitSpeed.knots)
+            weather.waves.direction = Measurement(value: Double.random(in: 0...360), unit: UnitAngle.degrees)
+            weather.waves.height = Measurement(value: Double.random(in: 0...3), unit: UnitLength.meters)
+            weather.conditions.title = "Sunny"
+            weather.conditions.symbolName = "sun.max"
+            weather.solar.isDaylight = true
+            
+            weatherObjects.append(weather)
+            
+            // Increment the date by one hour
+            date.addTimeInterval(3600)
+        }
+        
+        return weatherObjects
+    }
+}
